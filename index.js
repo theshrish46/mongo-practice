2beb40d6d8628263e9ecb52b856ba4bc106224ad
theshrish46 <theshrish46@gmail.com>
2023-05-30 23:33:27 +0530

    added some more files

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express()
-const port = 8080
+mongoose.connect("mongodb://127.0.0.1:27017/", { useNewUrlParser: true })
+    .then(() => console.log('Connected to the server successfully'))
+    .catch((err) => console.log(err))
 
+// Create a Schemea
+const StudentSchema = mongoose.Schema({
+    name: String,
+    roll: Number,
+    attendence: Boolean,
+    subjects: Array
+})
+
+// Create a Model
+const StudentModel = mongoose.model('StudentModel', StudentSchema)
+
+const shrish = StudentModel({
+    name: 'Shrish',
+    roll: 218,
+    attendence: true,
+    subjects: ['OST', 'OS', 'PYTHON', 'ANIMATION', 'KANNADA', 'ENGLISH']
+})
+
+
+shrish.save()
